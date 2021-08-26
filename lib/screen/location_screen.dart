d5a38e13a0d72e8b212947909e69ed0fc47b3406
Mohammed <yamni.zero@gmail.com>
2021-08-26 15:19:29 +0200

    choose city +

--- lib/screen/location_screen.dart
+++ lib/screen/location_screen.dart
@@ -5,6 +5,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:legacy_progress_dialog/legacy_progress_dialog.dart';
 import 'package:location/location.dart';
 
 class LocationScreen extends StatefulWidget {
@@ -21,7 +23,7 @@ class _LocationScreenState extends State<LocationScreen> {
   bool _serviceEnabled;
   PermissionStatus _permissionGranted;
   LocationData _locationData;
-
+  String _address;
 
   Future<LocationData> getLocation() async {
     _serviceEnabled = await location.serviceEnabled();
@@ -42,6 +44,18 @@ class _LocationScreenState extends State<LocationScreen> {
 
     _locationData = await location.getLocation();
 
+
+    //From coordinates
+    final coordinates = new Coordinates(_locationData.latitude,_locationData.longitude);
+   var addresses = await Geocoder.local.findAddressesFromCoordinates(coordinates);
+   var first = addresses.first;
+
+   setState(() {
+     _address = first.addressLine;
+     countryValue =first.countryName;
+
+   });
+
     return _locationData;
   }
 
@@ -54,123 +68,140 @@ class _LocationScreenState extends State<LocationScreen> {
   @override
   Widget build(BuildContext context) {
 
+    //Create an instance of ProgressDialog
+    ProgressDialog progressDialog = ProgressDialog(
+      context: context,
+      backgroundColor: Colors.white,
+      textColor: Colors.black,
+      loadingText: "Fetching location... ",
+      progressIndicatorColor: Theme.of(context).primaryColor,
+    );
+
     //ShowBottom
     showBottomScreen(context) {
-      showModalBottomSheet(
-          //need to make it full screen
-          isScrollControlled: true,
-          enableDrag: true,
-          context: context,
-          builder: (context) {
-            return Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                AppBar(
-                  automaticallyImplyLeading: false,
-                  iconTheme: IconThemeData(color: Colors.black),
-                  elevation: 1,
-                  backgroundColor: Colors.white,
-                  title: Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: Icon(Icons.clear),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Location",
-                        style: TextStyle(color: Colors.black),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(),
-                      borderRadius: BorderRadius.circular(6),
+
+      getLocation().then((location){
+
+        if(location!=null){
+          //only after fetch location only bottom screen will open
+          progressDialog.dismiss();
+          showModalBottomSheet(
+            //need to make it full screen
+              isScrollControlled: true,
+              enableDrag: true,
+              context: context,
+              builder: (context) {
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
                     ),
-                    child: SizedBox(
-                      height: 40,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Search City, area or neighbourhood",
-                          hintStyle: TextStyle(color: Colors.grey),
-                          icon: Icon(Icons.search),
+                    AppBar(
+                      automaticallyImplyLeading: false,
+                      iconTheme: IconThemeData(color: Colors.black),
+                      elevation: 1,
+                      backgroundColor: Colors.white,
+                      title: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(Icons.clear),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Location",
+                            style: TextStyle(color: Colors.black),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: SizedBox(
+                          height: 40,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "Search City, area or neighbourhood",
+                              hintStyle: TextStyle(color: Colors.grey),
+                              icon: Icon(Icons.search),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                ListTile(
-                  onTap: () {},
-                  horizontalTitleGap: 0.0,
-                  leading: Icon(
-                    Icons.my_location,
-                    color: Colors.blue,
-                  ),
-                  title: Text(
-                    "Use current location",
-                    style: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text(
-                    "Enable location",
-                    style: TextStyle(fontSize: 12),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  color: Colors.grey.shade300,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 14,bottom: 4,top: 4),
-                    child: Text(
-                      "CHOOSE CITY",
-                      style: TextStyle(color: Colors.blueGrey.shade900,fontSize: 12),
+                    ListTile(
+                      onTap: () {},
+                      horizontalTitleGap: 0.0,
+                      leading: Icon(
+                        Icons.my_location,
+                        color: Colors.blue,
+                      ),
+                      title: Text(
+                        "Use current location",
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        location == null ? "Fetching location" : _address,
+                        style: TextStyle(fontSize: 12),
+                      ),
                     ),
-                  ),
-                ),
-                //use a package to select Country  , state and city => csc_picker
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10,0,10,0),
-                  child: CSCPicker(
-                    layout: Layout.vertical,
-                    dropdownDecoration: BoxDecoration(shape: BoxShape.rectangle),
-                    defaultCountry: DefaultCountry.Sudan,
-                    onCountryChanged: (value) {
-                      setState(() {
-                        countryValue = value;
-                      });
-                    },
-                    onStateChanged:(value) {
-                      setState(() {
-                        stateValue = value;
-                      });
-                    },
-                    onCityChanged:(value) {
-                      setState(() {
-                        cityValue = value;
-                        address = '$cityValue, $stateValue, ${countryValue.substring(8)}';
-                      });
-                        print(address);
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.grey.shade300,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 14,bottom: 4,top: 4),
+                        child: Text(
+                          "CHOOSE CITY",
+                          style: TextStyle(color: Colors.blueGrey.shade900,fontSize: 12),
+                        ),
+                      ),
+                    ),
+                    //use a package to select Country  , state and city => csc_picker
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10,0,10,0),
+                      child: CSCPicker(
+                        layout: Layout.vertical,
+                        dropdownDecoration: BoxDecoration(shape: BoxShape.rectangle),
+                        defaultCountry: DefaultCountry.Sudan,
+                        onCountryChanged: (value) {
+                          setState(() {
+                            countryValue = value;
+                          });
+                        },
+                        onStateChanged:(value) {
+                          setState(() {
+                            stateValue = value;
+                          });
+                        },
+                        onCityChanged:(value) {
+                          setState(() {
+                            cityValue = value;
+                            _address = '$cityValue, $stateValue, ${countryValue.substring(8)}';
+                          });
+                        },
 
+                      ),
+                    ),
 
-                    },
-
-                  ),
-                ),
-
-              ],
-            );
-          });
+                  ],
+                );
+              });
+        }else{
+          progressDialog.dismiss();
+        }
+      });
     }
+
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -246,6 +277,7 @@ class _LocationScreenState extends State<LocationScreen> {
           ),
           InkWell(
             onTap: () {
+              progressDialog.show();
               showBottomScreen(context);
             },
             child: Padding(
