6f7e26d4d41c1e781eb0f7761bc99361ae25a8e9
Mohammed <yamni.zero@gmail.com>
2021-08-26 11:43:03 +0200

    choose city

--- lib/screen/location_screen.dart
+++ lib/screen/location_screen.dart
@@ -1,5 +1,6 @@
 import 'package:buysell/screen/home_screen.dart';
 import 'package:buysell/screen/login_screen.dart';
+import 'package:csc_picker/csc_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/cupertino.dart';
@@ -7,7 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
 class LocationScreen extends StatefulWidget {
-
   static const String id = "location-screen";
 
   @override
@@ -15,103 +15,250 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  bool _loading = false;
+  Location location = new Location();
+
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
+
+
+  Future<LocationData> getLocation() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return null;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    _locationData = await location.getLocation();
+
+    return _locationData;
+  }
+
+  // Variables to store country state city data in onChanged method.
+  String countryValue = "";
+  String stateValue = "";
+  String cityValue = "";
+  String address = "";
+
   @override
   Widget build(BuildContext context) {
 
-    bool _loading =false;
-    Location location = new Location();
-
-    bool _serviceEnabled;
-    PermissionStatus _permissionGranted;
-    LocationData _locationData;
-
-    Future<LocationData>getLocation() async {
-      _serviceEnabled = await location.serviceEnabled();
-      if (!_serviceEnabled) {
-        _serviceEnabled = await location.requestService();
-        if (!_serviceEnabled) {
-          return null;
-        }
-      }
-
-      _permissionGranted = await location.hasPermission();
-      if (_permissionGranted == PermissionStatus.denied) {
-        _permissionGranted = await location.requestPermission();
-        if (_permissionGranted != PermissionStatus.granted) {
-          return null;
-        }
-      }
-
-
-        _locationData = await location.getLocation();
-
-
-      return _locationData;
-    }
-
-
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Image.asset("assets/images/location.jpg"),
-          SizedBox(height: 50,),
-          Text("Where do  want\nto buy/sell products",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold ),
-          ),
-          SizedBox(height:10,),
-          Text("To enjoy all that we have to offer you\nwe need to know where to look for them",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 12),
-          ),
-          SizedBox(height: 30,),
-          Padding(
-            padding: const EdgeInsets.only(left: 20,right: 20,bottom: 10),
-            child: Row(
+    //ShowBottom
+    showBottomScreen(context) {
+      showModalBottomSheet(
+          //need to make it full screen
+          isScrollControlled: true,
+          enableDrag: true,
+          context: context,
+          builder: (context) {
+            return Column(
               children: [
-                Expanded(
-                  child: _loading ?
-                  Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),))
-                  :ElevatedButton.icon(
-                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).primaryColor)),
-                  icon: Icon(CupertinoIcons.location_fill),
-                    label: Padding(
-                      padding: const EdgeInsets.only(top: 15,bottom: 15),
-                      child: Text(
-                          "Around me",
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                SizedBox(
+                  height: 50,
+                ),
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  iconTheme: IconThemeData(color: Colors.black),
+                  elevation: 1,
+                  backgroundColor: Colors.white,
+                  title: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.clear),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Location",
+                        style: TextStyle(color: Colors.black),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: SizedBox(
+                      height: 40,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Search City, area or neighbourhood",
+                          hintStyle: TextStyle(color: Colors.grey),
+                          icon: Icon(Icons.search),
+                        ),
                       ),
                     ),
-                    onPressed: (){
-
-                     setState(() {
-                       _loading=true;
-                     });
-                      getLocation().then((value){
-                        if(value!=null){
-                          Navigator.pushReplacement(
-                              context, MaterialPageRoute(
-                            builder: (BuildContext context) => HomeScreen(
-                              locationData: _locationData,
-                            ),
-                          ),
-                          );
-                        }
+                  ),
+                ),
+                ListTile(
+                  onTap: () {},
+                  horizontalTitleGap: 0.0,
+                  leading: Icon(
+                    Icons.my_location,
+                    color: Colors.blue,
+                  ),
+                  title: Text(
+                    "Use current location",
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    "Enable location",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.grey.shade300,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 14,bottom: 4,top: 4),
+                    child: Text(
+                      "CHOOSE CITY",
+                      style: TextStyle(color: Colors.blueGrey.shade900,fontSize: 12),
+                    ),
+                  ),
+                ),
+                //use a package to select Country  , state and city => csc_picker
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10,0,10,0),
+                  child: CSCPicker(
+                    layout: Layout.vertical,
+                    dropdownDecoration: BoxDecoration(shape: BoxShape.rectangle),
+                    defaultCountry: DefaultCountry.Sudan,
+                    onCountryChanged: (value) {
+                      setState(() {
+                        countryValue = value;
                       });
                     },
+                    onStateChanged:(value) {
+                      setState(() {
+                        stateValue = value;
+                      });
+                    },
+                    onCityChanged:(value) {
+                      setState(() {
+                        cityValue = value;
+                        address = '$cityValue, $stateValue, ${countryValue.substring(8)}';
+                      });
+                        print(address);
+
+
+                    },
+
                   ),
                 ),
 
               ],
+            );
+          });
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Image.asset("assets/images/location.jpg"),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            "Where do  want\nto buy/sell products",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "To enjoy all that we have to offer you\nwe need to know where to look for them",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 12),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: _loading
+                      ? Center(
+                          child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              Theme.of(context).primaryColor),
+                        ))
+                      : ElevatedButton.icon(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Theme.of(context).primaryColor)),
+                          icon: Icon(CupertinoIcons.location_fill),
+                          label: Padding(
+                            padding: const EdgeInsets.only(top: 15, bottom: 15),
+                            child: Text(
+                              "Around me",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _loading = true;
+                            });
+                            getLocation().then((value) {
+                              if (value != null) {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        HomeScreen(
+                                      locationData: _locationData,
+                                    ),
+                                  ),
+                                );
+                              }
+                            });
+                          },
+                        ),
+                ),
+              ],
             ),
           ),
-          TextButton(
-              onPressed: (){},
-              child: Text(
-                  "set location manually",
-              style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,color: Colors.black,decoration: TextDecoration.underline),
-              )
+          InkWell(
+            onTap: () {
+              showBottomScreen(context);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration:
+                    BoxDecoration(border: Border(bottom: BorderSide(width: 2))),
+                child: Text(
+                  "Set location manually",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+            ),
           ),
         ],
       ),
