f1308242ff7a8b08062a43d2910d02d3c7658264
Mohammed <yamni.zero@gmail.com>
2021-09-30 10:59:50 +0200

    fin

--- lib/screen/chat/chat_conversation_screen.dart
+++ lib/screen/chat/chat_conversation_screen.dart
@@ -2,6 +2,8 @@ import 'package:buysell/services/firebase_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import 'chat_stream.dart';
+
 class ChatConversation extends StatefulWidget {
   final String chatRoomId;
   ChatConversation({this.chatRoomId});
@@ -14,7 +16,7 @@ class ChatConversation extends StatefulWidget {
 class _ChatConversationState extends State<ChatConversation> {
   FirebaseServices _services =FirebaseServices();
 
-Stream chatMessageStream;
+
 var chatMessageController = TextEditingController();
 
  bool _send =false;
@@ -31,50 +33,35 @@ sendMessage(){
   }
 }
 
-@override
-  void initState() {
-   _services.getChat(widget.chatRoomId).then((value){
-     setState(() {
-       chatMessageStream = value;
-     });
-
-   });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0.0,
+              backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.call),
+              onPressed: (){}
+              ),
+          IconButton(
+              icon: Icon(Icons.more_vert_sharp),
+              onPressed: (){}
+              ),
+        ],
+        shape: Border(bottom: BorderSide(color: Colors.grey)),
+      ),
       body: Container(
         child: Stack(
           children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: StreamBuilder<QuerySnapshot>(
-            stream: chatMessageStream,
-            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return Text('Something went wrong');
-              }
-
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              }
-
-              return snapshot.hasData ? ListView.builder(
-                itemCount: snapshot.data.docs.length,
-                  itemBuilder: (context,index){
-                  return Text(snapshot.data.docs[index]['message']);
-                  }
-              ): Container();
-            },
-        ),
-          ),
+            ChatStream(widget.chatRoomId),
             Container(
               alignment: Alignment.bottomCenter,
               child: Container(
                 decoration: BoxDecoration(
+                  color: Colors.white,
                   border:Border(
                     top: BorderSide(color: Colors.grey.shade800),
                   ),
@@ -101,6 +88,11 @@ sendMessage(){
                                 setState(() {
                                   _send = false;
                                 });
+                              }
+                            },
+                            onSubmitted: (value){
+                              if(value.length>0){
+                                sendMessage();
                               }
                             },
 
---
+++ lib/screen/chat/chat_stream.dart
@@ -0,0 +1,126 @@
+import 'package:buysell/services/firebase_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_bubble/bubble_type.dart';
+import 'package:flutter_chat_bubble/chat_bubble.dart';
+import 'package:flutter_chat_bubble/clippers/chat_bubble_clipper_2.dart';
+import 'package:intl/intl.dart';
+
+class ChatStream extends StatefulWidget {
+  final String chatRoomId;
+
+  ChatStream(this.chatRoomId);
+
+  @override
+  _ChatStreamState createState() => _ChatStreamState();
+}
+
+class _ChatStreamState extends State<ChatStream> {
+  FirebaseServices _services = FirebaseServices();
+
+  Stream chatMessageStream;
+  DocumentSnapshot chatDoc;
+  final _format = NumberFormat('##,##,##0');
+
+  @override
+  void initState() {
+    _services.getChat(widget.chatRoomId).then((value) {
+      setState(() {
+        chatMessageStream = value;
+      });
+    });
+    _services.messages.doc(widget.chatRoomId).get().then((value) {
+      setState(() {
+        chatDoc = value;
+      });
+    });
+    super.initState();
+  }
+
+  String _priceFormatted(price) {
+    var _price = int.parse(price);
+    var _formattedPrice = _format.format(_price);
+    return _formattedPrice;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: chatMessageStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        return snapshot.hasData
+            ? Column(
+                children: [
+                  if (chatDoc != null)
+                    ListTile(
+                      title: Text(chatDoc['product']['title']),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text('\$${_priceFormatted(
+                            chatDoc['product']['price'],
+                          )}'),
+                          SizedBox(
+                            width: 100,
+                          ),
+                        ],
+                      ),
+                    ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey.shade300,
+                      child: ListView.builder(
+                          itemCount: snapshot.data.docs.length,
+                          itemBuilder: (context, index) {
+                            String sentBy = snapshot.data.docs[index]['sentBy'];
+                            String me = _services.user.uid;
+                            //add chat time
+                            String lastChatDate;
+                            var _date=DateFormat.yMMMd().format(DateTime.fromMicrosecondsSinceEpoch(snapshot.data.docs[index]['time']));
+                            var _today=DateFormat.yMMMd().format(DateTime.fromMicrosecondsSinceEpoch(DateTime.now().microsecondsSinceEpoch));
+
+                            if(_date==_today){
+                              lastChatDate = DateFormat('hh:mm').format(DateTime.fromMicrosecondsSinceEpoch(snapshot.data.docs[index]['time']));
+                            }else{
+                              lastChatDate =_date.toString();
+                            }
+
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  ChatBubble(
+                                    backGroundColor: sentBy == me
+                                        ? Theme.of(context).primaryColor
+                                        : Colors.grey,
+                                    child: Text(
+                                      snapshot.data.docs[index]['message'],
+                                      style: TextStyle(color: sentBy==me?Colors.white:Colors.black),
+                                    ),
+                                    clipper: ChatBubbleClipper2(
+                                        type: BubbleType.sendBubble),
+                                  ),
+                                  Align(
+                                      alignment: sentBy==me? Alignment.centerRight :Alignment.centerLeft,
+                                      child: Text(lastChatDate)),
+                                ],
+                              ),
+                            );
+                          }),
+                    ),
+                  ),
+                ],
+              )
+            : Container();
+      },
+    );
+  }
+}
